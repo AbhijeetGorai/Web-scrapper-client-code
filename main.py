813a8806b783bbac0313549c40471ef25d402bbf
Abhijeet Gorai <50688988+AbhijeetGorai@users.noreply.github.com>
2025-02-03 20:49:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import autogen
 
 #Defining the OEPN AI Keys
 api_key = os.getenv("OPENAI_API_KEY")
-llm_config1 = {"model": "o3-mini","temperature" : 0}
+llm_config1 = {"model": "openai-o3-mini","temperature" : 0}
 
 #Defining the Firecrawl
 app = FirecrawlApp(api_key = os.getenv("FIRECRAWL_API_KEY"))
